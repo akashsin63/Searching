5182d35bd3def2731453f5b7d6be858227e53c21
Akash <akashsingh54812@gmail.com>
2023-08-20 00:03:12 +0530

    540. Single Element in a Sorted Array

--- singleElement.java
+++ singleElement.java
@@ -1,6 +1,6 @@
 public class singleElement {
     public static void main(String[] args) {
-        int[] arr = {1};
+        int[] arr = {1,1,2};
         System.out.println(binarySearch(arr));
     }
     public static int binarySearch(int[] A){
@@ -16,7 +16,7 @@ public class singleElement {
                     low = mid+1;
                 }
             }
-            else if(mid>0 && mid<A.length && A[mid]== A[mid+1]){
+            else if(mid>0 && mid<A.length-1 && A[mid]== A[mid+1]){
                 if(mid%2 != 0){
                     high = mid-1;
                 }else{
